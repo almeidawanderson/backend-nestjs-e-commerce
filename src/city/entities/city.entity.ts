81ecd688e18a73d8480410887c87163510eb9504
Wanderson Almeida <wandersonalmeida.eng@gmail.com>
2023-05-12 11:22:38 -0300

    feat: create feat cityEntity

--- src/city/entities/city.entity.ts
+++ src/city/entities/city.entity.ts
@@ -1,1 +1,24 @@
-export class City {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({name: 'city'})
+export class CityEntity {
+  @PrimaryGeneratedColumn('rowid')
+  city_id: number;
+
+  @Column({name: 'state_id', nullable: false})
+  state_id: number;
+
+  @Column({name: 'city_name', nullable: false})
+  city_name: number;
+
+  @CreateDateColumn({name: 'created_at'})
+  created_at: Date
+
+  @UpdateDateColumn({name: 'updated_at'})
+  updated_at: Date
+
+
+
+
+
+}
