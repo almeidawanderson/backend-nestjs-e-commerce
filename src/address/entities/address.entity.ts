7af37eec00121236e3d35afbd0deb7170c058b33
Wanderson Almeida <wandersonalmeida.eng@gmail.com>
2023-05-12 11:18:18 -0300

    feat: create feat addressEntity

--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -1,1 +1,33 @@
-export class Address {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+@Entity({name: 'address', schema: 'public'})
+export class Address {
+  @PrimaryGeneratedColumn('rowid')
+  address_id: number;
+
+  @Column({name: 'user_id', nullable: false})
+  user_id: number;
+
+  @Column({name: 'complement', nullable: true})
+  complement: number;
+
+  @Column({name: 'number', nullable: false})
+  number_address: number;
+
+  @Column({name: 'cep', nullable: false})
+  cep: string;
+
+  @Column({name: 'city_id', nullable: false})
+  city_id: number;
+
+  @CreateDateColumn({name: 'created_at'})
+  created_at: Date;
+
+  @UpdateDateColumn({name: 'updated_at'})
+  update_at: Date;
+
+  
+
+
+}
